16a5d1e13ea2313571caa5f6afb3bdd2a2600f4d
Tuan Huynh <tuanhuynh@pinterest.com>
2017-04-06 16:36:01 -0700

    update ChannelsList to process data prop, add Apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -10,11 +10,19 @@ import './App.css';
 
 const client = new ApolloClient();
 
-const ChannelsList = () =>
-  (<ul>
-    <li>Channel 1</li>
-    <li>Channel 2</li>
-  </ul>);
+const ChannelsList = ({ data: { loading, error, channels }}) => {
+  if (loading) {
+    return <p>Loading ...</p>
+  }
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+
+  return <ul>
+    { channels.map( ch => <li key={ch.id}>{ch.name}</li>)}
+  </ul>
+
+}
 
 const channelsListQuery = gql`
   query ChannelsListQuery {
@@ -35,13 +43,15 @@ const ChannelsListWithData = graphql(channelsListQuery)(ChannelsList);
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Apollo</h2>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to Apollo</h2>
+          </div>
+          <ChannelsListWithData />
         </div>
-        <ChannelsList />
-      </div>
+      </ApolloProvider>
     );
   }
 }
